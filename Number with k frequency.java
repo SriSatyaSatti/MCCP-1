f6502d5969f641c1cf95bb6572fd27b297003a05
Sri Satya Satti <102864137+SriSatyaSatti@users.noreply.github.com>
2023-09-19 18:39:26 +0530

    Update Number with k frequency.java

--- Number with k frequency.java
+++ Number with k frequency.java
@@ -1,4 +1,7 @@
-  static int firstElement(int arr[], int n, int k) { 
+// Find first element that is repeated k times
+import java.util.*;
+public class MyClass {
+    static int KFreq(int arr[],int n,int k){
      HashMap<Integer,Integer> h=new HashMap<>();
      for(int i=0;i<n;i++){
          if(h.containsKey(arr[i])){
@@ -14,3 +17,16 @@
      }
      return -1;
     }
+    public static void main(String args[]) {
+      Scanner sc=new Scanner(System.in);
+      int n=sc.nextInt();
+      int k=sc.nextInt();
+      int t;
+      int a[]=new int[n];
+      for(int i=0;i<n;i++){
+          a[i]=sc.nextInt();
+      }
+      t=KFreq(a,n,k);
+      System.out.print(t);
+    }
+}
