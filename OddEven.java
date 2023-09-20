4c6bd47a1c2e141a59b42467074e1d5abf3d2a6a
Sri Satya Satti <102864137+SriSatyaSatti@users.noreply.github.com>
2023-09-20 21:27:30 +0530

    Create OddEven.java

---
+++ OddEven.java
@@ -0,0 +1,30 @@
+//.Segregate even and odd numbers such that odd should come first followed by even
+
+import java.util.*;
+public class MyClass {
+    static ArrayList<Integer> move(int a[],int n){
+        ArrayList<Integer> t=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            if(a[i]%2!=0)
+            t.add(a[i]);
+        }
+        for(int i=0;i<n;i++){
+            if(a[i]%2==0)
+            t.add(a[i]);
+        }
+        return t;
+    }
+    public static void main(String args[]) {
+      Scanner sc=new Scanner(System.in);
+      int n=sc.nextInt();
+      ArrayList<Integer> t=new ArrayList<>();
+      int a[]=new int[n];
+      for(int i=0;i<n;i++){
+          a[i]=sc.nextInt();
+      }
+      t=move(a,n);
+      for(Integer x:t){
+          System.out.println(x+" ");
+      }
+    }
+}
