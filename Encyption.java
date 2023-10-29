9a0c7274b67e3c332c6840e7f463e1e4d854b800
Sri Satya Satti <102864137+SriSatyaSatti@users.noreply.github.com>
2023-10-29 20:20:42 +0530

    Create Encyption.java

---
+++ Encyption.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class MyClass {
+    static void encryption(int arr[],int n){
+    int max=arr[n-1];
+    arr[n-1]=0;
+    for(int i=n-2;i>=0;i--){
+        int t=arr[i];
+        arr[i]=max;
+        max=Math.max(max,t);
+    }
+}
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        encryption(arr,n);
+        for(int i=0;i<n;i++)
+            System.out.print(arr[i]+" ");
+    }
+}
