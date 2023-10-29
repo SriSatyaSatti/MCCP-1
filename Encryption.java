f1e96811fb2cec2aeffb46ea02f904388d7db45b
Sri Satya Satti <102864137+SriSatyaSatti@users.noreply.github.com>
2023-10-29 21:07:41 +0530

    Create Encryption.java

---
+++ Encryption.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class MyClass {
+    static void encryption(int arr[],int n){
+        int t[]=new int[n];
+        t[0]=arr[0]+arr[1];
+        t[n-1]=arr[n-1]+arr[n-2];
+        for(int i=1;i<n-1;i++){
+            t[i]=arr[i-1]+arr[i+1];
+        }
+        for(int i=0;i<n;i++){
+            arr[i]=t[i];
+        }
+}
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        encryption(arr,n);
+        for(int i=0;i<n;i++)
+            System.out.print(arr[i]+" ");
+    }
+}
