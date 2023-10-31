c6a7ec4281a5541108141a9257db6794777d8f60
Sri Satya Satti <102864137+SriSatyaSatti@users.noreply.github.com>
2023-10-31 21:35:33 +0530

    Create Pivot linear.java

---
+++ Pivot linear.java
@@ -0,0 +1,22 @@
+// Find pivot in rotated sorted array using Linear search
+import java.util.Scanner;
+public class MyClass {
+    static int numofrotations(int a[],int n){
+        if(a[0]<a[n-1])
+        return 0;
+        for(int i=1;i<n;i++){
+            if(a[i-1]>a[i])
+            return i;
+        }
+        return 0;
+    }
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+        }
+        System.out.println(numofrotations(a,n));
+    }
+}
