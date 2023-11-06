a9c0cf9f950a901419ddc6ed912634616d46e87e
Sri Satya Satti <102864137+SriSatyaSatti@users.noreply.github.com>
2023-11-06 21:55:00 +0530

    Create Occur only once-1.java

---
+++ Occur only once-1.java
@@ -0,0 +1,21 @@
+//Find the single element that occurs only once in the sorted array using Linear Search
+    import java.util.Scanner;
+    public class MyClass {
+                static int small(int a[], int n) {
+                    for (int i = 1; i < n; i += 2) {
+                        if (a[i - 1] != a[i])
+                            return a[i - 1];
+                    }
+                    return -1;
+                }
+
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+        }
+        System.out.println(small(a,n));
+    }
+}
