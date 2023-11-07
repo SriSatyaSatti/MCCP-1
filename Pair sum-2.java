a772f2543bfa833034780dd1ed69f3edfa50d301
Sri Satya Satti <102864137+SriSatyaSatti@users.noreply.github.com>
2023-11-07 21:03:00 +0530

    Create Pair sum-2.java

---
+++ Pair sum-2.java
@@ -0,0 +1,29 @@
+// Two sum problem for unsorted data using hashmap
+import java.util.Scanner;
+
+public class MyClass{
+    static int[] search(int a[],int n,int x){
+        int t[]=new int[2];
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(a[i]+a[j]==x){
+                   t[0]=i;
+                   t[1]=j;
+                }
+            }
+        }
+        return t;
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        int t[]=new int[2];
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+        }
+        int k=sc.nextInt();
+        t=search(a,n,k);
+        System.out.println(t[0]+" "+t[1]);
+    }
+}
